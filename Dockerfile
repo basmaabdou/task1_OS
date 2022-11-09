b0f78dcfd4d060e811881dfac74b4994adcad95d
Your Name <you@example.com>
2022-11-09 18:16:24 +0200

    add..

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM  openjdk
+
+WORKDIR /application
+
+COPY basma.java .
+
+RUN javac basma.java
+
+CMD java basma
---
+++ basma.java
@@ -0,0 +1,8 @@
+
+public class Basma {
+
+   
+    public static void main(String[] args) {
+        System.out.println("BASMA");
+    }
+} 
